ecc0646eaf53aaa533db86e78d0da53f32b903b4
Cailin Pitt <cailinpitt1@gmail.com>
2022-06-18 12:42:48 -0400

    cailin: add method comments

--- index.js
+++ index.js
@@ -8,8 +8,18 @@ const argv = require('minimist')(process.argv.slice(2));
 
 const assetDirectory = `./assets-${argv.location}-${uuidv4()}`;
 
+/**
+ * Temporarily halts program execution.
+ * @param {Number} ms number of miliseconds to wait
+ * @returns promise
+ */
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
+/**
+ * Makes a GET request to Citizen to fetch 200 recent incidents. Using 200 because I think that
+ * shgould be a high enough limit to grab all incidents for a given day.
+ * @returns JSON list of incidents.
+ */
 const fetchIncidents = async () => {
     const response = await axios({
         url: `https://citizen.com/api/incident/trending?lowerLatitude=${keys[argv.location].lowerLatitude}&lowerLongitude=${keys[argv.location].lowerLongitude}&upperLatitude=${keys[argv.location].upperLatitude}&upperLongitude=${keys[argv.location].upperLongitude}&fullResponse=true&limit=200`,
@@ -19,6 +29,12 @@ const fetchIncidents = async () => {
     return response.data.results
 };
 
+/**
+ * Makes a GET request to download a map image of an incident.
+ * @param {String} url url of the image to download
+ * @param {String} eventKey the ID of the citizen incident
+ * @returns resolved promise.
+ */
 const downloadMapImage = async (url, eventKey) => {
     const imagePath = path.resolve(__dirname, `${assetDirectory}/${eventKey}.png`);
     const writer = fs.createWriteStream(imagePath);
@@ -32,11 +48,19 @@ const downloadMapImage = async (url, eventKey) => {
     return new Promise(resolve => response.data.pipe(writer).on('finish', resolve));
 };
 
+/**
+ * Deletes asset folder from disk, and then re-creates it.
+ */
 const resetAssetsFolder = () => {
     fs.removeSync(assetDirectory);
     fs.ensureDirSync(assetDirectory);
 };
 
+/**
+ * Tweets information on a Citizen incident that includes a Pedestrian or Bicyclist
+ * @param {*} client the instantiated Twitter client
+ * @param {*} incident the Citizen incident to tweet
+ */
 const tweetIncident = async (client, incident) => {
     const incidentDate = new Date(incident.ts).toLocaleString('en-US', { timeZone: keys[argv.location].timeZone});
     const mediaId = await client.v1.uploadMedia(`${assetDirectory}/${incident.key}.png`);
